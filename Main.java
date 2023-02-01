8e34addc8eda54fac11b0f4a972463faf2d1fb82
pigcodes29 <103080686+pigcodes29@users.noreply.github.com>
2023-02-01 12:50:18 -0500

    Update code and add via upload

--- Main.java
+++ Main.java
@@ -2,15 +2,44 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Main{
-	public static void main(String args[]) throws IOExeption {
+	public static void main(String args[]) {
         //system objects
         Scanner in = new Scanner(System.in);
-        Randon rand = new Random();
+        Random rand = new Random();
 
         //Game Variables
-        String[] enemies = { "Slime", "Creeper", "Zombie", "Villager", "Dream" } //dream is the boss, villager is for genocide route
+        String[] enemies = { "Slime", "Creeper", "Zombie", "Villager"}; //villager is for genocide route
         int maxEnemyHealth = 45;
         int maxAttackDamage = 10;
 
+        int health = 50;
+        int AttackDamage = 12;
+        int numHeathPots = 3;
+        int healthPotHealAmmount = 30;
+        int healthPotionDropChance = 35;
+
+        boolean running = true;
+
+        System.out.println("Welcome to TerminalCraft Legends!");
+
+        GAME:
+        while(running) {
+                System.out.println("----------------------------------------");
+
+                int enemyHealth = rand.nextInt(maxEnemyHealth);
+                String enemy = enemies[rand.nextInt(enemies.length)];
+                System.out.println("\t# " + enemy + " has appeared! #\n");
+
+                while(enemyHealth > 0) {
+                        System.out.println("\tYour HP: " + health);
+                        System.out.println("\t" + enemy + "'s HP: " + enemyHealth);
+                        System.out.println("\n\tWhat Would You Like to Do?");
+                        System.out.println("\t1. Attack");
+                        System.out.println("\t2. Drink Health");
+                        System.out.println("\t3. Flee");
+
+                        String input = in.nextLine();
+                }
+        }
 	}
 }
