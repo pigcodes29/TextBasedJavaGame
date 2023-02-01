e6314fa9fe8bd361937f6e1c6c4d35429ece7302
pigcodes29 <103080686+pigcodes29@users.noreply.github.com>
2023-02-01 12:34:03 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main{
+	public static void main(String args[]) throws IOExeption {
+        //system objects
+        Scanner in = new Scanner(System.in);
+        Randon rand = new Random();
+
+        //Game Variables
+        String[] enemies = { "Slime", "Creeper", "Zombie", "Villager", "Dream" } //dream is the boss, villager is for genocide route
+        int maxEnemyHealth = 45;
+        int maxAttackDamage = 10;
+
+	}
+}
